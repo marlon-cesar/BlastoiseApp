04c4b6e3e710ffe922f8efc06fd6f9792b038d6a
marlon-cesar <marlon.cesar@yahoo.com>
2025-04-14 19:08:03 -0300

    config sweet alert

---
+++ BlastoiseApp.Web/wwwroot/js/alert.js
@@ -0,0 +1,46 @@
+﻿function successAlert(title, text) {
+	Swal.fire({
+		title: title,
+		text: text,
+		icon: "success",
+		confirmButtonText: "Ok",
+	})
+}
+
+function errorAlert(title, text) {
+	Swal.fire({
+		title: title,
+		text: text,
+		icon: "error",
+		confirmButtonText: "Ok",
+	})
+}
+
+var toastMixin = Swal.mixin({
+	toast: true,
+	animation: true,
+	position: 'top-right',
+	showConfirmButton: false,
+	timer: 3000,
+	timerProgressBar: true,
+	didOpen: (toast) => {
+		toast.addEventListener('mouseenter', Swal.stopTimer)
+		toast.addEventListener('mouseleave', Swal.resumeTimer)
+	}
+});
+
+
+function toastSuccess(title, text) {
+	toastMixin.fire({
+		icon: 'success',
+		title: title,
+		text: text
+	})
+}
+function toastError(title, text) {
+	toastMixin.fire({
+		icon: 'error',
+		title: title,
+		text: text
+	})
+}
