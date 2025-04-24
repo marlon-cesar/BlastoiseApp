b90b9d534dd410738ad8876c27d8daf45c415b40
Gabriela Esmaniotto <gabriela.esmaniotto@yahoo.com>
2025-04-24 17:29:51 -0300

    Criado tela de contato

---
+++ BlastoiseApp.Web/Controllers/ContactController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlastoiseApp.Web.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+	
+	  }
+
+}
